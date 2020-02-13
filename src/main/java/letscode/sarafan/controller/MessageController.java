7ccc01bf3d76591ff3d40e7245e51dc182e48474
me <some_email>
2020-02-13 03:50:43 +0300

    4: add mysql, refactoring

--- src/main/java/letscode/sarafan/controller/MessageController.java
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -1,60 +1,50 @@
 package letscode.sarafan.controller;
 
-import letscode.sarafan.exceptions.NotFoundException;
+import letscode.sarafan.domain.Message;
+import letscode.sarafan.repo.MessageRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    private int counter = 4;
-    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "FirstMessage"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "SecondMessage"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "ThirdMessage"); }});
-    }};
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public MessageController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping
-    public List<Map<String, String>> list() {
-        return messages;
+    public List<Message> list() {
+        return messageRepo.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getOne(@PathVariable String id) {
-        return getMessage(id);
+    public Message getOne(@PathVariable("id") Message message) {
+        return message;
     }
 
     // fetch('/message', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify( { text: 'Fourth message' } ) }).then(console.log)
     @PostMapping
-    public Map<String, String> create(@RequestBody Map<String, String> message) {
-        message.put("id", String.valueOf(counter++));
-        messages.add(message);
-        return message;
+    public Message create(@RequestBody Message message) {
+        return messageRepo.save(message);
     }
 
     // fetch('/message/4', { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify( { text: 'Fourth message (4)', id: '10' } ) }).then(console.log)
     @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
-        Map<String, String> messageFromDb = getMessage(id);
-        messageFromDb.putAll(message);
-        messageFromDb.put("id", id);
-        return messageFromDb;
+    public Message update(@PathVariable("id") Message messageFromDb,
+                                      @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+        return messageRepo.save(messageFromDb);
     }
 
     // fetch('/message/4', { method: 'DELETE' }).then(console.log)
     @DeleteMapping("{id}")
-    public void delete(@PathVariable String id) {
-        Map<String, String> message = getMessage(id);
-        messages.remove(message);
-    }
-
-    private Map<String, String> getMessage(@PathVariable String id) {
-        return messages.stream().filter(message -> message.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
+    public void delete(@PathVariable("id") Message message) {
+        messageRepo.delete(message);
     }
 }
